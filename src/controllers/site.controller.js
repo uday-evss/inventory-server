0178af1622faba9521624cc4c5f6c57280f72d29
uday-evss <uday.evss@gmail.com>
2026-02-06 10:39:37 +0530

    site detailed screen

--- src/controllers/site.controller.js
+++ src/controllers/site.controller.js
@@ -1,7 +1,7 @@
 import db from "../models/index.js";
 // import sequelize from "../config/database.js";
 
-const { SiteData } = db;
+const { SiteData, AssetRequest, AssetRequestItem, AssetReturnRequest, AssetReturnItem, AssetRequestItemImage, AssetReturnImage, User, Asset } = db;
 
 //CREATING A SITE
 export const createSite = async (req, res, next) => {
@@ -59,16 +59,159 @@ export const deleteSite = async (req, res, next) => {
 
 
 // GET SITE BY ID
+// export const getSiteById = async (req, res, next) => {
+//     try {
+//         const site = await SiteData.findByPk(req.params.id);
+//         if (!site) return res.status(404).json({ message: "Site not found" });
+
+//         res.json({ data: site });
+//     } catch (err) {
+//         next(err);
+//     }
+// };
+
 export const getSiteById = async (req, res, next) => {
     try {
-        const site = await SiteData.findByPk(req.params.id);
-        if (!site) return res.status(404).json({ message: "Site not found" });
+        const site = await SiteData.findByPk(req.params.id, {
+            attributes: [
+                "site_id",
+                "bridge_no",
+                "location",
+                "site_division",
+                "site_last_date",
+            ],
+            include: [
+                {
+                    model: AssetRequest,
+                    as: "assetRequests",
+                    where: {
+                        admin_approval: "APPROVED",
+                        allocated: 1,
+                    },
+                    required: false,
+                    include: [
+                        {
+                            model: User,
+                            as: "requestedBy",
+                            attributes: ["id", "fullName", "username"],
+                        },
+                        {
+                            model: User,
+                            as: "approvedBy",
+                            attributes: ["id", "fullName"],
+                        },
+                        {
+                            model: AssetRequestItem,
+                            as: "items",
+                            required: false,
+                            include: [
+                                {
+                                    model: Asset,
+                                    as: "asset",
+                                    attributes: [
+                                        "asset_id",
+                                        "asset_name",
+                                        "units",
+                                        "asset_type",
+                                        "asset_condition",
+                                        "asset_status",
+                                        "remarks",
+
+                                    ],
+                                },
+                                {
+                                    model: AssetRequestItemImage,
+                                    as: "images",
+                                    attributes: [
+                                        "id",
+                                        "image_url",
+                                        "usage_qty",
+                                        "asset_condition",
+                                        "uploaded_at",
+                                    ],
+                                    separate: true,
+                                    order: [["uploaded_at", "DESC"]],
+                                },
+                                {
+                                    model: AssetReturnRequest,
+                                    as: "returnRequests",
+                                    attributes: [
+                                        "return_id",
+                                        "request_item_id",
+                                        "status",
+                                        "return_type",
+                                        "receiver_remarks",
+                                    ],
+                                    include: [
+                                        {
+                                            model: SiteData,
+                                            as: "fromSite",
+                                            attributes: [
+                                                "site_id",
+                                                "bridge_no",
+                                                "location",
+                                            ],
+                                        },
+                                        {
+                                            model: SiteData,
+                                            as: "toSite",
+                                            attributes: [
+                                                "site_id",
+                                                "bridge_no",
+                                                "location",
+                                            ],
+                                        },
+                                        {
+                                            model: AssetReturnItem,
+                                            as: "items",
+                                            attributes: [
+                                                "id",
+                                                "return_qty",
+                                                "asset_id",
+                                            ],
+                                            include: [
+                                                {
+                                                    model: AssetReturnImage,
+                                                    as: "images",
+                                                    attributes: [
+                                                        "id",
+                                                        "image_url",
+                                                        "stage",
+                                                        "asset_condition",
+                                                        "uploaded_at",
+                                                    ],
+                                                },
+                                                {
+                                                    model: Asset,
+                                                    as: "asset",
+                                                    attributes: [
+                                                        "asset_id",
+                                                        "asset_name",
+                                                        "units",
+                                                    ],
+                                                },
+                                            ],
+                                        },
+                                    ],
+                                },
+                            ],
+                        },
+                    ],
+                },
+            ],
+        });
+
+        if (!site) {
+            return res.status(404).json({ message: "Site not found" });
+        }
 
         res.json({ data: site });
     } catch (err) {
         next(err);
     }
 };
+
+
 
 // UPDATE SITE
 export const updateSite = async (req, res, next) => {
