028a2ed649a83f3adcecdb0ec773295139ca7a3f
uday-evss <uday.evss@gmail.com>
2026-02-11 14:50:07 +0530

    multi tenancy user table uniqueness

--- src/models/User.model.js
+++ src/models/User.model.js
@@ -13,6 +13,7 @@ const User = sequelize.define(
         employeeId: {
             type: DataTypes.STRING,
             allowNull: false,
+            // unique: true,
         },
 
         fullName: {
